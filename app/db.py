c6eaf8af72381147cf10439d6542c7588e67283a
xirf <itsandka@gmail.com>
2024-07-08 15:28:13 +0700

    init project

--- app/db.py
+++ app/db.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
